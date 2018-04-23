f2f90fae36c5ac2f79b83d831807f5e6cf765b55
Jirat Pasuksmit <jirat@ampostech.com>
2018-04-23 23:27:45 +0700

    add query 100 row script

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+
+try:
+    import urllib.request
+except ImportError:
+    import pip
+    pip.main(['install', 'urllib'])
+    try:
+        import urllib
+    except ImportError:
+        sys.exit('Error ! using $ pip install urllib')
+
+jira_url = "jira.spring.io"
+base_url = "https://{project}/sr/jira.issueviews:searchrequest{fields}/temp/SearchRequest.csv?{query}"
+file_type = ".csv"
+
+
+def build_filename(project_url, count):
+    from time import gmtime, strftime
+    time_text = strftime("%Y%m%d_%H%M%S", gmtime())
+    return project_url + "_" +  time_text + "_" + str(count) + "rows." + file_type
+
+
+def add_parameter(key, value):
+    return "jqlQuery=" + "&" + key + "=" + value
+
+
+def build_query_url(project_url, query_all_field, count):
+    query_fields = "-csv-all-fields" if query_all_field else "-csv-current-fields"
+    query_properties = add_parameter("tempMax", str(count))
+    return base_url.format(project=project_url, fields=query_fields, query=query_properties)
+
+
+if __name__ == '__main__':
+    row_count = 100
+    url = build_query_url(jira_url, True, row_count)
+    test_file = urllib.request.urlretrieve(url, build_filename(jira_url, row_count))

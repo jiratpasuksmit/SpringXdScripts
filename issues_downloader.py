41a95087a12311fd10548528da7eea94d669a69c
Jirat Pasuksmit <jirat@ampostech.com>
2018-04-26 05:42:24 +0700

    reformat code

--- issues_downloader.py
+++ issues_downloader.py
@@ -1,15 +1,4 @@
-import sys
-
-try:
-    import urllib.request
-except ImportError:
-    import pip
-
-    pip.main(['install', 'urllib'])
-    try:
-        import urllib
-    except ImportError:
-        sys.exit('Error ! using $ pip install urllib')
+import urllib.request
 
 jira_url = "jira.spring.io"
 base_url = "https://{project}/sr/jira.issueviews:searchrequest{fields}/temp/SearchRequest.csv?{query}"
@@ -22,20 +11,9 @@ def build_filename(project_url, count):
     return project_url + "_" + time_text + "_" + str(count) + "rows" + file_type
 
 
-def add_parameter(key, value):
-    params_string = "jqlQuery="
-    if type(key) is list and type(value) is list:
-        for k, v in key, value:
-            params_string += "&{k}={v}".format(k=k, v=v)
-    else:
-        params_string += "&{k}={v}".format(k=key, v=value)
-
-    return params_string
-
-
 def build_query_url(project_url, query_all_field, count):
     query_fields = "-csv-all-fields" if query_all_field else "-csv-current-fields"
-    query_properties = add_parameter(["tempMax", str(count)], ["a", 123])
+    query_properties = "jqlQuery=&project=XD&tempMax={count}&status=(closed,done,resolved)".format(count=count)
     return base_url.format(project=project_url, fields=query_fields, query=query_properties)
 
 
